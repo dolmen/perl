ab02ea104d8b934fcfc9863f66a52f1cb7162d74
Karl Williamson <public@khwilliamson.com>
2013-02-14 10:50:00 -0700

    Fix some EBCDIC problems
    These spots have native code points, so should be using the macros for
    native code points, instead of Unicode ones.

--- regcomp.c
+++ regcomp.c
@@ -10951,7 +10951,7 @@ tryagain:
                          * character using utf8.  If we start to fold non-UTF
                          * patterns, be sure to update join_exact() */
                         if (LOC && ender < 256) {
-                            if (UNI_IS_INVARIANT(ender)) {
+                            if (NATIVE_IS_INVARIANT(ender)) {
                                 *s = (U8) ender;
                                 foldlen = 1;
                             } else {
